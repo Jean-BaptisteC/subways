4e1574c2af57819c5d9f85ddb1bd18d54325b957
Jean-Baptiste <87148630+Jean-BaptisteC@users.noreply.github.com>
2025-08-11 04:47:04 +0200

    Update validation.py
    Signed-off-by: Jean-Baptiste <87148630+Jean-BaptisteC@users.noreply.github.com>

--- subways/validation.py
+++ subways/validation.py
@@ -6,7 +6,7 @@ from functools import partial
 from subways.structure.city import City
 from subways.types import CriticalValidationError, LonLat, OsmElementT
 
-DEFAULT_SPREADSHEET_ID = "1tXHFXWqkQNrpQxSLX88RglTkpsA5PHMFsfwQ1nfau60"
+DEFAULT_SPREADSHEET_ID = "1PIPH1jEG9-zWq599HjYDuM2Sk4pN5pE2jj9mdX7g67E"
 DEFAULT_CITIES_INFO_URL = (
     "https://docs.google.com/spreadsheets/d/"
     f"{DEFAULT_SPREADSHEET_ID}/export?format=csv"
