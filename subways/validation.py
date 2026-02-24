15559fd7a993b5deb8b194fa6cf94451b68b9803
Jean-Baptiste <87148630+Jean-BaptisteC@users.noreply.github.com>
2026-02-24 22:14:26 +0100

    Update default spreadsheet ID in validation.py
    Signed-off-by: Jean-Baptiste <87148630+Jean-BaptisteC@users.noreply.github.com>

--- subways/validation.py
+++ subways/validation.py
@@ -6,7 +6,7 @@ from functools import partial
 from subways.structure.city import City
 from subways.types import CriticalValidationError, LonLat, OsmElementT
 
-DEFAULT_SPREADSHEET_ID = "1PIPH1jEG9-zWq599HjYDuM2Sk4pN5pE2jj9mdX7g67E"
+DEFAULT_SPREADSHEET_ID = "1SEW1-NiNOnA2qDwievcxYV1FOaQl1mb1fdeyqAxHu3k"
 DEFAULT_CITIES_INFO_URL = (
     "https://docs.google.com/spreadsheets/d/"
     f"{DEFAULT_SPREADSHEET_ID}/export?format=csv"
